4d27ce059edfbe66ce580b3c0da160c019c04887
Kasper <591307@stud.hvl.no>
2022-05-01 14:15:30 +0200

    d

--- ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,16 +1,39 @@
 package no.hvl.dat110.aciotdevice.client;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.google.gson.Gson;
+
 public class RestClient {
 
 	public RestClient() {
 		// TODO Auto-generated constructor stub
 	}
 
-	private static String logpath = "/accessdevice/log";
+	private static String logpath = "/accessdevice/log/";
+	private static String host = "http://localhost:8080";
 
 	public void doPostAccessEntry(String message) {
-
-		// TODO: implement a HTTP POST on the service to post the message
+		
+			HttpClient client = HttpClient.newHttpClient();
+	        HttpRequest request = HttpRequest.newBuilder()
+	                .uri(URI.create(host+logpath))
+	                .POST(HttpRequest.BodyPublishers.ofString(new Gson().toJson(new AccessMessage(message))))
+	                .build();
+			
+			try {
+				client.send(request, HttpResponse.BodyHandlers.ofString());
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		// implement a HTTP POST on the service to post the message
 		
 	}
 	
@@ -20,7 +43,19 @@ public class RestClient {
 
 		AccessCode code = null;
 		
-		// TODO: implement a HTTP GET on the service to get current access code
+		try {
+			URL url = new URL(host+codepath);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestMethod("GET");
+			con.setDoInput(true);
+			code = new Gson().fromJson(new String(con.getInputStream().readAllBytes()), AccessCode.class);
+			con.disconnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// implement a HTTP GET on the service to get current access code
 		
 		return code;
 	}
